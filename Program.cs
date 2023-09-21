295a57e642f3ab211807972fd9c240253c027220
balogun14 <awwalbalogun06@gmail.com>
2023-09-21 09:54:33 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.Write("Enter Text: ");
+// string text = Console.ReadLine()!;
+
+// if (text == "Hello World!")
+// {
+//     System.Console.WriteLine($"{text} is correct");
+// }
+// else
+// {
+//     System.Console.WriteLine($"{text}  does not match");
+// }
+
+Random random = new();
+int a = random.Next(5, 50);
+int b = random.Next(5, 50);
+
+if (a < b)
+{
+    System.Console.WriteLine($"{a} is less than {b}");
+}
+
+else if (a > b)
+{
+    System.Console.WriteLine($"{a} is greater than {b}");
+}
+else
+{
+    System.Console.WriteLine($"{a} is equals to {b}");
+}
